3f658ba0fb62f8d4d6dd863c16007bef2b846315
Timo Wang <timowang1991@gmail.com>
2025-07-26 17:35:06 +0800

    jupyter notebook to demonstrate l2 distance between embeddings

--- section4-facts/prompt.py
+++ section4-facts/prompt.py
@@ -2,7 +2,11 @@ from langchain.vectorstores.chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
+from redundant_filter_retriever import RedundantFilterRetriever
 from dotenv import load_dotenv
+import langchain
+
+langchain.debug = True  # Enable debug mode for detailed logging
 
 load_dotenv()
 
@@ -11,7 +15,10 @@ embeddings = OpenAIEmbeddings()
 
 db = Chroma(persist_directory="emb", embedding_function=embeddings)
 
-retriever = db.as_retriever()
+retriever = RedundantFilterRetriever(
+    embeddings=embeddings,
+    chroma=db
+)
 
 chain = RetrievalQA.from_chain_type(
     llm=chat,
